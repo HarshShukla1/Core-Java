caffa5e3e841c5bf1f027cda4b4b5cdaa8b88420
Harsh Shukla <harshshukla24112000@gmail.com>
2026-05-07 23:05:03 +0530

    this and super

---
+++ ThisandSuper.java
@@ -0,0 +1,35 @@
+class A
+{
+    public A()
+    {
+        System.out.println("in A");
+    }
+    public A(int n)
+    {
+        System.out.println("in A int");
+    }
+
+}
+
+class B extends A
+{
+    public B()
+    {
+        super();
+        System.out.println("in B");
+    }
+    public B(int n)
+    {
+        super(n);
+        System.out.println("in B int");
+    }
+
+}
+
+
+
+public class ThisandSuper {
+    public static void main(String[] args) {
+        B obj = new B(5);
+    }
+}
